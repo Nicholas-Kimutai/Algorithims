30a18b0bfbfb03fc5921fdd003dc28bd8d8add6a
Nicholas-Kimutai <kimutainick74@gmail.com>
2022-09-09 23:36:39 +0300

    Initial commit

---
+++ src/Validation.java
@@ -0,0 +1,21 @@
+public class Validation {
+
+    //Validation algorithm for uppercase
+    public static boolean isUppercase(String s){
+        return s.chars().allMatch(Character::isUpperCase);
+
+    }
+
+    //validation for lowercase
+    public static boolean isLowercase(String s){
+        return s.chars().allMatch(Character::isLowerCase);
+    }
+    public static void main(String[] args) {
+        System.out.println(isUppercase("phenomenal"));
+        System.out.println(isUppercase("PHENOMENAL"));
+
+        System.out.println(isLowercase("phenomenal"));
+        System.out.println(isLowercase("PHENOMENAL"));
+
+    }
+}
